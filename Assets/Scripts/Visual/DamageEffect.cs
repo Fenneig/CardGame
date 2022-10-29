a16953df724604311547ca16d7cc8b4726c037f1
Fenneig <ninetailedkot92@gmail.com>
2022-10-29 06:09:27 +0300

    add damage effect and fix global settings script

---
+++ Assets/Scripts/Visual/DamageEffect.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace CardGame.Visual
+{
+    public class DamageEffect : MonoBehaviour
+    {
+        [SerializeField] private Sprite[] _splashes;
+        [SerializeField] private Image _damageImage;
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private Text _amountText;
+        [Header("Fade details")]
+        [SerializeField] private float _timeTillTextFade = 1f;
+        [SerializeField] private float _fadeTickTime = 0.05f;
+        [SerializeField] private float _fadeTickAmount = 0.05f;
+
+        public Text AmountText
+        {
+            get => _amountText;
+            set => _amountText = value;
+        }
+
+        private void Awake()
+        {
+            _damageImage.sprite = _splashes[Random.Range(0, _splashes.Length)];
+        }
+
+        private IEnumerator ShowDamageEffect()
+        {
+            _canvasGroup.alpha = 1f;
+
+            yield return new WaitForSeconds(_timeTillTextFade);
+
+            while (_canvasGroup.alpha > 0)
+            {
+                _canvasGroup.alpha -= _fadeTickAmount;
+                yield return new WaitForSeconds(_fadeTickTime);
+            }
+
+            Destroy(gameObject);
+        }
+
+        public static void CreateDamageEffect(Vector3 position, int amount)
+        {
+            GameObject newDamageEffect = Instantiate(GlobalSettings.Instance.DamageEffectPrefab, position, quaternion.identity);
+            DamageEffect damageEffect = newDamageEffect.GetComponent<DamageEffect>();
+            damageEffect.AmountText.text = $"-{amount.ToString()}";
+            damageEffect.StartCoroutine(damageEffect.ShowDamageEffect());
+        }
+    }
+}
---
+++ Assets/Scripts/Visual/GlobalSettings.cs
@@ -0,0 +1,26 @@
+﻿using CardGame.Logic;
+using UnityEngine;
+
+namespace CardGame.Visual
+{
+    public class GlobalSettings : MonoBehaviour
+    {
+        [Header("Player")]
+        [SerializeField] private Player _topPlayer;
+        [SerializeField] private Player _lowPlayer;
+        [Header("Colors")] 
+        [SerializeField] private Color32 _cardBodyStandardColor;
+        [SerializeField] private Color32 _cardRibbonsStandardColor;
+        [SerializeField] private Color32 _cardGlowColor;
+        [SerializeField] private GameObject _damageEffectPrefab;
+
+        private static GlobalSettings _instance;
+        public static GlobalSettings Instance => _instance;
+        public GameObject DamageEffectPrefab => _damageEffectPrefab;
+
+        private void Awake()
+        {
+            _instance = this;
+        }
+    }
+}
