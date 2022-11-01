64327fd0069a05aff28e6890ddcfed96f0a75091
Fenneig <ninetailedkot92@gmail.com>
2022-11-01 06:49:59 +0300

    create message manager

---
+++ Assets/Scripts/Visual/MessageManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using CardGame.Commands;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CardGame.Visual
+{
+    public class MessageManager : MonoBehaviour
+    {
+        [SerializeField] private Text _messageText;
+        [SerializeField] private GameObject _messagePanel;
+
+        public static MessageManager Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+            _messagePanel.SetActive(false);
+        }
+
+        public void ShowMessage(string message, float duration) =>
+            StartCoroutine(ShowMessageCoroutine(message, duration));
+
+        private IEnumerator ShowMessageCoroutine(string message, float duration)
+        {
+            _messageText.text = message;
+            _messagePanel.SetActive(true);
+
+            yield return new WaitForSeconds(duration);
+
+            _messagePanel.SetActive(false);
+            // TODO Command.CommandExecutionComplete();
+        }
+    }
+}
